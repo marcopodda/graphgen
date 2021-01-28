8a701965d8aa93c99bfd7b485019d4054bec7c45
Marco Podda <marco.podda@di.unipi.it>
2021-01-28 12:11:52 +0100

    ok

--- main.py
+++ main.py
@@ -29,50 +29,34 @@ if __name__ == '__main__':
 
     random.seed(123)
 
-    graphs = create_graphs(args)
+    # graphs = create_graphs(args)
+    splits = args.current_dataset_path / "graphs" / "splits.dat"
 
-    # random.shuffle(graphs)
-    # graphs_train = graphs[: int(0.80 * len(graphs))]
+    graphs = args.current_dataset_path / "graphs" / "graphs.dat"
+    tensors = args.current_dataset_path / "min_dfscode_tensors" / "min_dfscode_tensors.dat"
+
     # graphs_validate = graphs[int(0.80 * len(graphs)): int(0.90 * len(graphs))]
 
-    # # show graphs statistics
-    # print('Model:', args.note)
-    # print('Device:', args.device)
-    # print('Graph type:', args.graph_type)
-    # print('Training set: {}, Validation set: {}'.format(
-    #     len(graphs_train), len(graphs_validate)))
+    # show graphs statistics
+    print('Model:', args.note)
+    print('Device:', args.device)
+    print('Graph type:', args.graph_type)
 
-    # # Loading the feature map
-    # with open(args.current_dataset_path / 'map.dict', 'rb') as f:
-    #     feature_map = pickle.load(f)
+    # Loading the feature map
+    with open(args.current_dataset_path / 'map.dict', 'rb') as f:
+        feature_map = pickle.load(f)
 
-    # print('Max number of nodes: {}'.format(feature_map['max_nodes']))
-    # print('Max number of edges: {}'.format(feature_map['max_edges']))
-    # print('Min number of nodes: {}'.format(feature_map['min_nodes']))
-    # print('Min number of edges: {}'.format(feature_map['min_edges']))
-    # print('Max degree of a node: {}'.format(feature_map['max_degree']))
-    # print('No. of node labels: {}'.format(len(feature_map['node_forward'])))
-    # print('No. of edge labels: {}'.format(len(feature_map['edge_forward'])))
+    print('Max number of nodes: {}'.format(feature_map['max_nodes']))
+    print('Max number of edges: {}'.format(feature_map['max_edges']))
+    print('Min number of nodes: {}'.format(feature_map['min_nodes']))
+    print('Min number of edges: {}'.format(feature_map['min_edges']))
+    print('Max degree of a node: {}'.format(feature_map['max_degree']))
+    print('No. of node labels: {}'.format(len(feature_map['node_forward'])))
+    print('No. of edge labels: {}'.format(len(feature_map['edge_forward'])))
 
-    # # Setting max_prev_node / max_tail_node and max_head_node
-    # if args.note == 'GraphRNN':
-    #     start = time.time()
-    #     if args.max_prev_node is None:
-    #         args.max_prev_node = calc_max_prev_node(
-    #             args.current_processed_dataset_path)
+    dataset_train = Graph_DFS_code_from_file(tensors, splits['train'])
+    dataset_validate = Graph_DFS_code_from_file(tensors, splits['val'])
 
-    #     args.max_head_and_tail = None
-    #     print('max_prev_node:', args.max_prev_node)
-
-    #     end = time.time()
-    #     print('Time taken to calculate max_prev_node = {:.3f}s'.format(
-    #         end - start))
-
-    # if args.note == 'DFScodeRNN' and args.weights:
-    #     feature_map = {
-    #         **feature_map,
-    #         **dfscodes_weights(args.min_dfscode_path, graphs_train, feature_map, args.device)
-    #     }
 
     # if args.note == 'GraphRNN':
     #     random_bfs = True
@@ -81,10 +65,7 @@ if __name__ == '__main__':
     #     dataset_validate = Graph_Adj_Matrix_from_file(
     #         args, graphs_validate, feature_map, random_bfs)
     # elif args.note == 'DFScodeRNN':
-    #     dataset_train = Graph_DFS_code_from_file(
-    #         args, graphs_train, feature_map)
-    #     dataset_validate = Graph_DFS_code_from_file(
-    #         args, graphs_validate, feature_map)
+
     # elif args.note == 'DGMG':
     #     dataset_train = DGMG_Dataset_from_file(args, graphs_train, feature_map)
     #     dataset_validate = DGMG_Dataset_from_file(
