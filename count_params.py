5e249f82e2335e91fdbf12856b85ce13af9b4e7f
Marco Podda <marco.podda@di.unipi.it>
2021-02-02 23:45:38 +0100

    ok

--- count_params.py
+++ count_params.py
@@ -7,12 +7,12 @@ from graphgen.model import create_model
 from utils import load_model, get_model_attribute
 
 
-def count_params(eval_args):
+def count_params(path, eval_args):
     train_args = eval_args.train_args
     feature_map = get_model_attribute('feature_map', eval_args.model_path, eval_args.device)
     model = create_model(train_args, feature_map)
     load_model(eval_args.model_path, eval_args.device, model)
-    filename = train_args.current_model_save_path / "num_params.txt"
+    filename = path.parent / "num_params.txt"
     num_params = sum([sum(p.numel() for p in model[m].parameters() if p.requires_grad) for m in model])
     with open(filename, "w") as f:
         print(num_params, file=f)
@@ -23,5 +23,6 @@ if __name__ == "__main__":
     parser.add_argument("--path", required=True)
     args = parser.parse_args()
 
-    eval_args = ArgsEvaluate(Path(args.path))
-    count_params(eval_args)
+    path = Path(args.path)
+    eval_args = ArgsEvaluate(path)
+    count_params(path, eval_args)
